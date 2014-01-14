558b80e8e138c8e1a751eb8e52d41d9be33c5ba6
jeando <jean-do.favreau@hotmail.fr>
2014-01-14 18:40:50 +0100

    a

--- perceptron.m
+++ perceptron.m
@@ -1,15 +1,27 @@
-function w = perceptron(sigma,sigma_prim,in,out, epsilon, max_iter)
-	w = rand(size(out),1);
-	delta_w = zeros(size(out),1);
+function w = perceptron(in,out, epsilon, max_iter)
+function result = sigma(x)
+    result = 1./(1+exp(-x));
+end
+
+function result = sigma_p(x)
+    s = sigma(x);
+    %result=1;
+    result = s.*(1-s);
+end
+
+
+	w = rand(size(in,2),1)*2-1;
+    w
+	delta_w = zeros(size(in,2),1);
 	iter=0;
 	while iter < max_iter
 		delta_w = delta_w * 0;
 		in_w = in * w;
-		for i = 1 : size(out)
-			delta_w(i) = delta_w(i) + epsilon*(in_w-out)*'in(:,i) * sigma_prim(in_w);
+		for i = 1 : size(w)
+			delta_w(i) = delta_w(i) + epsilon*((out-in_w)'*(in(:,i))) * sigma_p(in_w(i,1));
 		end
 		w = w + delta_w;
-		iter++
+		iter = iter + 1;
 	end
 end
 
