c23b1205ceb58845702f20aad69bf1d6323b4aa3
allaeia <marie.guenon@laposte.net>
2014-02-11 15:14:15 +0100

    f

--- TP4/TP4.m
+++ TP4/TP4.m
@@ -1,18 +1,25 @@
 %% TP4
 
+clc;
+clear all;
+close all;
 
-img = imread('le_cervin.png');
+img = double(imread('le_cervin.png'));
 [w h] = size(img);
 
 %% Question 1
 % save random seed as key
-key = rng
+key = rng('default');
 % generate random noise 
-u = randn(w, h);
+u = double(randn(w, h));
 
-imagesc(u); colormap('gray'); colorbar;
+figure; imagesc(img); colormap('gray'); colorbar; title('Image d''origine');
+figure; imagesc(u); colormap('gray'); colorbar; title('Bruit blanc');
 
 
-
+%% Question 2
+gamma = 1;
+w = img + gamma .* (-1).^floor(u);
+figure; imshow(w, [0 255]); colorbar; title('Image marquee'); 
 
 
