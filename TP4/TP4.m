e3a3485464618a985674df0c418fe381727e8c98
TANGUY Arnaud <arn.tanguy@gmail.com>
2014-02-11 15:25:58 +0100

    ..

--- TP4/TP4.m
+++ TP4/TP4.m
@@ -1,5 +1,6 @@
 %% TP4
 
+function TP4()
 clc;
 clear all;
 close all;
@@ -9,7 +10,7 @@ img = double(imread('le_cervin.png'));
 
 %% Question 1
 % save random seed as key
-key = rng('default');
+key= rng;
 % generate random noise 
 u = double(randn(w, h));
 
@@ -19,7 +20,24 @@ figure; imagesc(u); colormap('gray'); colorbar; title('Bruit blanc');
 
 %% Question 2
 gamma = 1;
-w = img + gamma .* (-1).^floor(u);
+w = img + gamma .* (-1).^floor(u) * u;
+%w = img + gamma .* u;
 figure; imshow(w, [0 255]); colorbar; title('Image marquee'); 
+
+figure; imshow(abs(w-img)*20, [0 255]); title('Difference entre image marquee et image d''origine (x20)');
+
+
+%% Question3
+function dec = decode(key, img)
+    rng(key);
+    % regenerate the same noise 
+    u = double(randn(w, h));
+end
+
+
+
+end
+
+
 
 
