cbc8f3393c5f73ae08160a3f3d01289ce203d3d9
TANGUY Arnaud <arn.tanguy@gmail.com>
2014-02-11 14:58:47 +0100

    .

--- TP4/TP4.m
+++ TP4/TP4.m
@@ -1,18 +1,23 @@
 %% TP4
 
+clc;
+clear all;
+close all;
 
 img = imread('le_cervin.png');
 [w h] = size(img);
 
 %% Question 1
 % save random seed as key
-key = rng;
+key = rng('default');
 % generate random noise 
 u = randn(w, h);
 
-imagesc(u); colormap('gray'); colorbar;
+figure; imagesc(img); colormap('gray'); colorbar; title('Image d''origine');
+figure; imagesc(u); colormap('gray'); colorbar; title('Bruit blanc');
 
 
+%% Question 2
 
 
 
