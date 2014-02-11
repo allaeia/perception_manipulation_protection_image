bbb6ab55ad7eed10c427e0d737d2622145eb52c9
TANGUY Arnaud <arn.tanguy@gmail.com>
2014-02-11 14:52:21 +0100

    Q1

--- TP4/TP4.m
+++ TP4/TP4.m
@@ -1,1 +1,18 @@
 %% TP4
+
+
+img = imread('le_cervin.png');
+[w h] = size(img);
+
+%% Question 1
+% save random seed as key
+key = rng;
+% generate random noise 
+u = randn(w, h);
+
+imagesc(u); colormap('gray'); colorbar;
+
+
+
+
+
